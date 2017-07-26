c5255aef9091734ba386a24c63ad5a5ed87f7e8f
Mahesh Waghmare <mwaghmare7@gmail.com>
2017-07-26 19:15:26 +0530

    Reused file download logic.

---
+++ importers/class-astra-server-helper.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Astra Site Server Helper
+ *
+ * @since  1.0.0
+ * @package Astra Sites Server
+ */
+
+if( ! class_exists( 'Astra_Sites_Server_Helper' ) ) :
+
+	/**
+	 * Astra_Sites_Server_Helper
+	 *
+	 * @since 1.0.0
+	 */
+	class Astra_Sites_Server_Helper {
+
+		/**
+		 * Instance
+		 *
+		 * @access private
+		 * @since 1.0.0
+		 */
+		private static $instance;
+
+		/**
+		 * Initiator
+		 *
+		 * @since 1.0.0
+		 * @return object initialized object of class.
+		 */
+		public static function get_instance(){
+			if ( ! isset( self::$instance ) ) {
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * Constructor
+		 *
+		 * @since 1.0.0
+		 */
+		public function __construct() {
+		}
+
+		/**
+		 * Download File Into Uploads Directory
+		 *
+		 * @param  string $file Download File URL.
+		 * @return array 		Downloaded file data.
+		 */
+		public static function download_file( $file = '' ) {
+
+			// Gives us access to the download_url() and wp_handle_sideload() functions.
+			require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+			$timeout_seconds = 5;
+
+			// Download file to temp dir.
+			$temp_file = download_url( $file, $timeout_seconds );
+
+			// WP Error.
+			if( is_wp_error( $temp_file ) ) {
+				return array(
+					'success' => false,
+					'data'    => $temp_file->get_error_message(),
+				);
+			}
+
+			// Array based on $_FILE as seen in PHP file uploads
+			$file_args = array(
+				'name'     => basename( $file ),
+				'tmp_name' => $temp_file,
+				'error'    => 0,
+				'size'     => filesize( $temp_file ),
+			);
+
+			$overrides = array(
+
+				// Tells WordPress to not look for the POST form
+				// fields that would normally be present as
+				// we downloaded the file from a remote server, so there
+				// will be no form fields
+				// Default is true
+				'test_form' => false,
+
+				// Setting this to false lets WordPress allow empty files, not recommended
+				// Default is true
+				'test_size' => true,
+
+				// A properly uploaded file will pass this test. There should be no reason to override this one.
+				'test_upload' => true,
+
+			);
+
+			// Move the temporary file into the uploads directory
+			$results = wp_handle_sideload( $file_args, $overrides );
+
+			if ( in_array( 'error', $results ) ) {
+				return array(
+					'success' => false,
+					'data'    => $results,
+				);
+			}
+
+			// Success!
+			return array(
+				'success' => true,
+				'data'    => $results,
+			);
+		}
+
+	}
+
+	/**
+	 * Kicking this off by calling 'get_instance()' method
+	 */
+	Astra_Sites_Server_Helper::get_instance();
+
+endif;
