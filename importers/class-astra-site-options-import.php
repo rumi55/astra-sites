579df8fa45f68bdb839e1e5c9712e43a7e78b0c5
Mahesh Waghmare <mwaghmare7@gmail.com>
2017-07-31 16:33:37 +0530

    Enhancement.

--- importers/class-astra-site-options-import.php
+++ importers/class-astra-site-options-import.php
@@ -64,29 +64,35 @@ class Astra_Site_Options_Import {
 		}
 
 		if( class_exists( 'SiteOrigin_Widgets_Bundle' ) ) {
-			vl( 'yes' );
+			// vl( 'yes' );
 
-			self::$default_active_widgets = $options['siteorigin_widgets_active'];
+			self::$default_active_widgets = $options['available_widgets'];
+			// vl( self::$default_active_widgets );
 
-			// foreach ( $options['siteorigin_widgets_active'] as $key => $value) {
-			// 	if( 1 == $value || '1' == $value ) {
-			// 		$temp[$key] = true;
-			// 	} else {
-			// 		$temp[$key] = false;
-			// 	}
-			// }
-			wp_cache_flush();
-			// Siteorigin Widgets
-			$active_widgets = wp_cache_get( 'active_widgets', 'siteorigin_widgets' );
-			vl( $active_widgets );
-			vl( $temp );
-			vl( get_option( 'siteorigin_widgets_active', '' ) );
-			SiteOrigin_Widgets_Bundle::$default_active_widgets = $temp;
-			wp_cache_add( 'active_widgets', $temp, 'siteorigin_widgets' );
-			update_option( 'siteorigin_widgets_active', $temp );
-			add_filter( 'siteorigin_widgets_default_active', array( $this, 'set_default_widgets' ) );
-			// create the initial single
-			$ob = new SiteOrigin_Widgets_Bundle();
+			// // foreach ( $options['siteorigin_widgets_active'] as $key => $value) {
+			// // 	if( 1 == $value || '1' == $value ) {
+			// // 		$temp[$key] = true;
+			// // 	} else {
+			// // 		$temp[$key] = false;
+			// // 	}
+			// // }
+			// wp_cache_flush();
+			// // Siteorigin Widgets
+			// $active_widgets = wp_cache_get( 'active_widgets', 'siteorigin_widgets' );
+			// vl( $active_widgets );
+			// vl( $temp );
+			// vl( get_option( 'siteorigin_widgets_active', '' ) );
+			// SiteOrigin_Widgets_Bundle::$default_active_widgets = $temp;
+			// wp_cache_add( 'active_widgets', $temp, 'siteorigin_widgets' );
+			// update_option( 'siteorigin_widgets_active', $temp );
+			// add_filter( 'siteorigin_widgets_default_active', array( $this, 'set_default_widgets' ) );
+			// // create the initial single
+			// $ob = new SiteOrigin_Widgets_Bundle();
+
+			add_filter( 'wie_available_widgets', array( $this, 'available_widgets' ), 8 );
+			update_option( 'siteorigin_widgets_active', $options['siteorigin_widgets_active'] );
+			wp_cache_delete( 'active_widgets', 'siteorigin_widgets' );
+
 			// $ob::single();
 			// $ob = new WP_Customize_Widgets();
 			// $ob->customize_controls_init();
@@ -101,6 +107,38 @@ class Astra_Site_Options_Import {
 		// Insert Logo.
 		$this->insert_logo( $options['custom_logo'] );
 	}
+
+	function available_widgets( $defaults = array() ) {
+
+		vl( 'BEFORE' );
+		vl( $defaults );
+
+		$merged = wp_parse_args( self::$default_active_widgets, $defaults );
+		vl( 'MERGED' );
+		vl( $merged );
+
+		return $merged;
+
+	}
+
+	// apply_filters( 'wie_available_widgets', $available_widgets );
+
+	// vl( $available_widgets );
+	// $available_widgets['sow-social-media-buttons'] = array(
+	//        'id_base' => 'sow-social-media-buttons',
+	//        'name' => 'SiteOrigin Social Media Buttons',
+	//    );
+	// $available_widgets['sow-features'] = array(
+	//        'id_base' => 'sow-features',
+	//        'name' => 'SiteOrigin Features',
+	//    );
+	// $available_widgets['sow-cta'] = array(
+	//        'id_base' => 'sow-cta',
+	//        'name' => 'SiteOrigin Call-to-action',
+	//    );
+
+	// vl( $available_widgets );
+	// wp_die();
 
 	function set_default_widgets( $defaults = array() ) {
 
--- importers/class-widgets-importer.php
+++ importers/class-widgets-importer.php
@@ -47,9 +47,9 @@ class Astra_Widget_Importer {
 
 		$widget_controls = $wp_registered_widget_controls;
 
-		vl( '-----------------------------------------------' );
-		vl( $widget_controls );
-		wp_die();
+		// vl( '-----------------------------------------------' );
+		// vl( $widget_controls );
+		// wp_die();
 
 		$available_widgets = array();
 
@@ -62,6 +62,9 @@ class Astra_Widget_Importer {
 
 			}
 		}
+
+		vl( 'AFTER' );
+		vl( $available_widgets );
 
 		return apply_filters( 'wie_available_widgets', $available_widgets );
 	}
@@ -79,7 +82,7 @@ class Astra_Widget_Importer {
 	function import_widgets_data( $data ) {
 
 		global $wp_registered_sidebars;
-		vl( $wp_registered_sidebars );
+		// vl( $wp_registered_sidebars );
 
 		// Have valid data?
 		// If no data or could not decode
@@ -99,7 +102,7 @@ class Astra_Widget_Importer {
 
 		// Get all available widgets site supports
 		$available_widgets = $this->wie_available_widgets();
-		vl( $available_widgets );
+
 		// $available_widgets['sow-social-media-buttons'] = array(
 	 //        'id_base' => 'sow-social-media-buttons',
 	 //        'name' => 'SiteOrigin Social Media Buttons',
@@ -139,13 +142,13 @@ class Astra_Widget_Importer {
 			// Check if sidebar is available on this site
 			// Otherwise add widgets to inactive, and say so
 			if ( isset( $wp_registered_sidebars[ $sidebar_id ] ) ) {
-				vl( $sidebar_id . ' = TRUE ' );
+				// vl( $sidebar_id . ' = TRUE ' );
 				$sidebar_available    = true;
 				$use_sidebar_id       = $sidebar_id;
 				$sidebar_message_type = 'success';
 				$sidebar_message      = '';
 			} else {
-				vl( $sidebar_id . ' = FALSE ' );
+				// vl( $sidebar_id . ' = FALSE ' );
 				$sidebar_available    = false;
 				$use_sidebar_id       = 'wp_inactive_widgets'; // add to inactive if sidebar does not exist in theme
 				$sidebar_message_type = 'error';
@@ -186,10 +189,10 @@ class Astra_Widget_Importer {
 				// It is probably much more likely that arrays are used than objects, however
 				$widget = json_decode( wp_json_encode( $widget ), true );
 
-				vl( '$id_base' );
-				vl( $id_base );
-				vl( '$widget' );
-				vl( $widget );
+				// vl( '$id_base' );
+				// vl( $id_base );
+				// vl( '$widget' );
+				// vl( $widget );
 
 				// Filter to modify settings array
 				// This is preferred over the older wie_widget_settings filter above
@@ -301,7 +304,7 @@ class Astra_Widget_Importer {
 		// Hook after import
 		do_action( 'wie_after_import' );
 
-		vl( $results );
+		// vl( $results );
 
 		// Return results
 		return apply_filters( 'wie_import_results', $results );
