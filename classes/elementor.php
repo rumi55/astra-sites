b10eeab80bbbfd96b14d7db327c85900a8c735a0
Mahesh Waghmare <mwaghmare7@gmail.com>
2017-08-09 18:35:05 +0530

    Ehnancement

--- classes/elementor.php
+++ classes/elementor.php
@@ -1,23 +1,77 @@
 <?php
+namespace Elementor;
+
+/**
+ * MY CLASS NAME
+ *
+ * @package MY CLASS NAME
+ * @since 1.0.0
+ */
+
+if( ! class_exists( 'TestMeElemenotr' ) ) :
+
+	/**
+	 * TestMeElemenotr
+	 *
+	 * @since 1.0.0
+	 */
+	class TestMeElemenotr {
+
+		/**
+		 * Instance
+		 *
+		 * @access private
+		 * @since 1.0.0
+		 */
+		private static $instance;
+
+		/**
+		 * Initiator
+		 *
+		 * @since 1.0.0
+		 * @return object initialized object of class.
+		 */
+		public static function set_instance(){
+			if ( ! isset( self::$instance ) ) {
+				self::$instance = new self;
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * Constructor
+		 *
+		 * @since 1.0.0
+		 */
+		public function __construct() {
+			add_action( 'admin_init', array( $this, 'get_attachments' ) );
+		}
+
+		public function get_attachments() {
+			$all_attachments = array();
+			$attachments = get_posts( array(
+			    'posts_per_page' => -1,
+			    'post_type' => 'attachment',
+			) );
+			// vl( $attachments );
+			foreach ($attachments as $key => $attachment) {
+				$settings = array(
+					'id' => $attachment->ID,
+					'url'  => $attachment->guid,
+				);
+				$settings = Plugin::$instance->templates_manager->get_import_images_instance()->import( $settings );
+				// vl( $settings );
+			}
+			// wp_die( );
+		}
 
 
-function get_attachments() {
-	$all_attachments = array();
-	$attachments = get_posts( array(
-	    'posts_per_page' => -1,
-	    'post_type' => 'attachment',
-	) );
-	// vl( $attachments );
-	foreach ($attachments as $key => $attachment) {
-		vl( $attachment->ID );
-		vl( $attachment->guid );
 	}
-	$all_attachments = array();
-	wp_die( );
 
-	// $settings = Plugin::$instance->templates_manager->get_import_images_instance()->import( $settings );
-	// vl( $settings );
+	/**
+	 * Kicking this off by calling 'set_instance()' method
+	 */
+	// TestMeElemenotr::set_instance();
 
-}
-add_action( 'admin_init', 'get_attachments' );
+endif;
 
