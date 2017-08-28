9b544846ba18d366c2134fcd9152401ca61d9f76
Mahesh Waghmare <mwaghmare7@gmail.com>
2017-08-28 15:46:44 +0530

    Enhancement.

--- astra-sites.php
+++ astra-sites.php
@@ -14,10 +14,38 @@
 /**
  * Set constants.
  */
-define( 'ASTRA_SITES_VER',  '1.0.4' );
-define( 'ASTRA_SITES_FILE', __FILE__ );
-define( 'ASTRA_SITES_BASE', plugin_basename( ASTRA_SITES_FILE ) );
-define( 'ASTRA_SITES_DIR',  plugin_dir_path( ASTRA_SITES_FILE ) );
-define( 'ASTRA_SITES_URI',  plugins_url( '/', ASTRA_SITES_FILE ) );
+if( ! defined( 'ASTRA_SITES_VER' ) ) {
+	define( 'ASTRA_SITES_VER',  '1.0.4' );
+}
 
-require_once ASTRA_SITES_DIR . 'classes/class-astra-sites.php';
+if( ! defined( 'ASTRA_SITES_FILE' ) ) {
+	define( 'ASTRA_SITES_FILE', __FILE__ );
+}
+
+if( ! defined( 'ASTRA_SITES_BASE' ) ) {
+	define( 'ASTRA_SITES_BASE', plugin_basename( ASTRA_SITES_FILE ) );
+}
+
+if( ! defined( 'ASTRA_SITES_DIR' ) ) {
+	define( 'ASTRA_SITES_DIR',  plugin_dir_path( ASTRA_SITES_FILE ) );
+}
+
+if( ! defined( 'ASTRA_SITES_URI' ) ) {
+	define( 'ASTRA_SITES_URI',  plugins_url( '/', ASTRA_SITES_FILE ) );
+}
+
+/**
+ * Set constants.
+ *
+ * @since 1.0.0
+ */
+if( ! function_exists( 'astra_sites_setup' ) ) :
+
+	function astra_sites_setup() {
+		require_once ASTRA_SITES_DIR . 'classes/class-astra-sites.php';
+	}
+
+	add_action( 'plugins_loaded', 'astra_sites_setup' );
+
+endif;
+
